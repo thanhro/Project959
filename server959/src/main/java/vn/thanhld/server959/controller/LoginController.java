390f1f8f059a727f8fc7da3cbfdb0082b10d9d8e
thanhro <lethanh309@gmail.com>
2019-08-07 01:36:27 +0700

    Initial Project client

--- server959/src/main/java/vn/thanhld/server959/controller/LoginController.java
+++ server959/src/main/java/vn/thanhld/server959/controller/LoginController.java
@@ -1,6 +1,5 @@
 package vn.thanhld.server959.controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
