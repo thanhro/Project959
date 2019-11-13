a56b2bf81dd6248dc5c9690310639605e19f7c57
Nguyễn Hưng Tuấn <tuan.nguyen1@ntq-solution.com.vn>
2019-11-13 15:02:55 +0700

    update code

--- client959/src/rooms/Rooms.js
+++ client959/src/rooms/Rooms.js
@@ -19,6 +19,97 @@ class rooms extends Component {
                     <div className="footer-rooms">
                     </div>
                 </div>
+                <div className="rooms ml-3 mt-3 mr-3">
+                    <div className="header-rooms col-sm-auto">
+                        <div className="img-color ">
+                            <div className="ml-2 pt-2">
+                                <h5>Sinhvien</h5>
+                                <h6>hello1</h6>
+                                <h6>0 sinh vien</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="footer-rooms">
+                    </div>
+                </div>
+                <div className="rooms ml-3 mt-3 mr-3">
+                    <div className="header-rooms col-sm-auto">
+                        <div className="img-color ">
+                            <div className="ml-2 pt-2">
+                                <h5>Sinhvien</h5>
+                                <h6>hello1</h6>
+                                <h6>0 sinh vien</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="footer-rooms">
+                    </div>
+                </div>
+                <div className="rooms ml-3 mt-3 mr-3">
+                    <div className="header-rooms col-sm-auto">
+                        <div className="img-color ">
+                            <div className="ml-2 pt-2">
+                                <h5>Sinhvien</h5>
+                                <h6>hello1</h6>
+                                <h6>0 sinh vien</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="footer-rooms">
+                    </div>
+                </div>
+                <div className="rooms ml-3 mt-3 mr-3">
+                    <div className="header-rooms col-sm-auto">
+                        <div className="img-color ">
+                            <div className="ml-2 pt-2">
+                                <h5>Sinhvien</h5>
+                                <h6>hello1</h6>
+                                <h6>0 sinh vien</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="footer-rooms">
+                    </div>
+                </div>
+                <div className="rooms ml-3 mt-3 mr-3">
+                    <div className="header-rooms col-sm-auto">
+                        <div className="img-color ">
+                            <div className="ml-2 pt-2">
+                                <h5>Sinhvien</h5>
+                                <h6>hello1</h6>
+                                <h6>0 sinh vien</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="footer-rooms">
+                    </div>
+                </div>
+                <div className="rooms ml-3 mt-3 mr-3">
+                    <div className="header-rooms col-sm-auto">
+                        <div className="img-color ">
+                            <div className="ml-2 pt-2">
+                                <h5>Sinhvien</h5>
+                                <h6>hello1</h6>
+                                <h6>0 sinh vien</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="footer-rooms">
+                    </div>
+                </div>
+                <div className="rooms ml-3 mt-3 mr-3">
+                    <div className="header-rooms col-sm-auto">
+                        <div className="img-color ">
+                            <div className="ml-2 pt-2">
+                                <h5>Sinhvien</h5>
+                                <h6>hello1</h6>
+                                <h6>0 sinh vien</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="footer-rooms">
+                    </div>
+                </div>
             </div>
         );
     }
